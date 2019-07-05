519c3fbf59f2b6e90c02814af2f145612b51ac02
Alaa ezzeldin <alaa.ezzeldin96@gmail.com>
2019-07-05 14:57:47 +0200

    landing page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component ,Input, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { Router,ActivatedRoute} from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -12,14 +14,41 @@ import { Router,ActivatedRoute} from '@angular/router';
 
 export class AppComponent implements OnInit{
 
+  token: string = "initial";
+
   constructor(
     private router: Router,
-    private activeRoute : ActivatedRoute
+    private activeRoute : ActivatedRoute,
+    private location: Location
+
+
   ) {}
 
   ngOnInit(): void {
-    if(localStorage.getItem('token')) console.log(this.activeRoute);
-    else this.router.navigate(['/register']);
+    if(localStorage.getItem('token')) {
+      // console.log(this.activeRoute);
+      console.log(window.location.pathname);
+
+      if( window.location.pathname ==='/**' ){
+        this.router.navigateByUrl('/error', {skipLocationChange: true});
+        this.location.replaceState('');
+      }
+      else if( window.location.pathname ==='/'){
+        this.router.navigateByUrl('/home', {skipLocationChange: true});
+        this.location.replaceState('');
+      }
+
+    }
+    // else if(this.router.url === '/home'){
+    //   this.router.navigateByUrl('/home', {skipLocationChange: true});
+    //   this.location.replaceState(''); 
+    // }
+    else{
+      this.router.navigateByUrl('/landing', {skipLocationChange: true});
+      this.location.replaceState('');
+    } 
+
+
   }
 
   
